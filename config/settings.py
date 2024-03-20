9a890d739fe4a0d903ed27d3bd2739130f0b0a4c
Javohir0913 <pro100java01@gmail.com>
2024-03-20 21:13:38 +0500

    cls

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ if DEBUG is False:
 else:
     STATICFILES_DIRS = [
         BASE_DIR / 'static',
-        BASE_DIR / 'image']
+        BASE_DIR / 'media']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
